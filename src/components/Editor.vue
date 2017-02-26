0fc6984e6adff245984d24ea4f5eef325b46e211
kangsLee <leeknsa@gmail.com>
2017-02-26 11:59:25 +0900

    init

---
+++ src/components/Editor.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="editor">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'editor',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods: {
+      handleSelect (key, keyPath) {
+        console.log(key, keyPath)
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
+</style>
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      EasyEditor Navigations.
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+
+    }
+  }
+</script>
+
