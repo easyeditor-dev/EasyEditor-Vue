e8fb9bf26041384d2979fe8f045d50ee4e814513
Taehwan Kim <maxtortime@gmail.com>
2017-02-26 14:47:01 +0900

    Add firebase config and init firebase

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import NavBar from 'components/NavBar'
+import * as firebase from 'firebase'
 
 Vue.use(ElementUI)
 Vue.component('navbar', NavBar)
@@ -16,3 +17,14 @@ new Vue({
   router,
   render: h => h(App)
 })
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: 'AIzaSyC2TLBwX_OGeaUNpJzxy_WuxTgAxR7ud84',
+  authDomain: 'easyeditor-adaa5.firebaseapp.com',
+  databaseURL: 'https://easyeditor-adaa5.firebaseio.com',
+  storageBucket: 'easyeditor-adaa5.appspot.com'
+}
+
+firebase.initializeApp(config)
